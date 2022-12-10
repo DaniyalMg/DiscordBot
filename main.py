d6903be47f8ea89d52be8132c94d285a114941e6
Daniyal <99318891+DaniyalMg@users.noreply.github.com>
2022-12-10 21:57:00 +0330

    add command clear to bot

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import discord
 import random
 import Data
+from discord.ext import commands
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+#client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='!', intents=intents)
 
 bad_words = ['avazi', 'khafe sho', 'bi tarbiat', 'bi shoor', 'oskol']
 reply_choice = ['khodeti!', "bi tarbiat!", 'chendesh', 'dige tekrar nashe']
@@ -25,6 +28,12 @@ async def on_message(message):
         await message.delete()
         await message.channel.send(f"{message.author.mention}" + " " + random.choice(reply_choice))
 
+    await client.process_commands(message)
+
+
+@client.command()
+async def clear(ctx, arg):
+    await ctx.channel.purge(limit=(int(arg) + 1))
+
 
 client.run(Data.token)
-
